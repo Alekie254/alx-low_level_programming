f88fb76476439d806a5abfa8616e658114379ddb
Alex Juma <j.alex9011@gmail.com>
2022-09-26 13:21:58 -0700

    8-print_diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,7 +12,6 @@ void print_diagsums(int *a, int size)
 int s;
 int diagonal1 = 0;
 int diagonal2 = 0;
-
 for (s = 0; s < size; s++)
 diagonal1 = diagonal1 + a[(size + 1) * s];
 for (s = 1; s < size + 1; s++)
